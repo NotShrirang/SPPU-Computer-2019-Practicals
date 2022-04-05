6f0a87bfb9914d076315a62a96b75b0388c3a725
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-04-05 08:05:21 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -14,18 +14,71 @@
 #         4       |
 #         5       |
 
-address_book = {}
+import sqlite3
+from numpy import int64
+
+conn = sqlite3.connect('num_dir.db')
+c = conn.cursor()
+c.execute("""CREATE TABLE if not exists directory (
+        name text,
+        number integer
+        )""")
+c.close()
+conn.commit()
+conn.close()
+
 def hash(name):
     y = ''
     for ch in name:
         ch = bytes(ch, 'utf-8')
+        # print(type(ch)," - ",ch)
         s = str(ch[0])
-        y += s[2]
+        # print(type(s)," - ",s)
+        y += s
+    # print(y)
     return y
+def linearProbingWithoutReplacement(name):
+    return(int64(name)+1)
+def search(name):
+    name = hash(name)
+    conn = sqlite3.connect('num_dir.db')
+    c = conn.cursor()
+    c.execute(f"SELECT number FROM directory WHERE name={name}")
+    record = c.fetchall()
+    c.close()
+    conn.commit()
+    conn.close()
+    print(record)
+    if record:
+        print(record[0])
+        return(1)
+    else:
+        print("returned 0")
+        return(0)
+def insert(name, num):
+    hname = hash(name)
+    conn = sqlite3.connect('num_dir.db')
+    c = conn.cursor()
+    if(search(name)==0):
+        c.execute("INSERT INTO directory VALUES (:name, :number)",
+                {
+                    'name' : str(hname),
+                    'number' : int(num)
+                })
+        c.close()
+    else:
+        print(hname)
+        hname = linearProbingWithoutReplacement(hname)
+        print(hname)
+        c.execute("INSERT INTO directory VALUES (:name, :number)",
+                {
+                    'name' : str(hname),
+                    'number' : int(num)
+                })
+        c.close()
+    conn.commit()
+    conn.close()
 
-def insert(index, num):
-    address_book[index] = num
+insert("Shrirang", 9404797231)
+# search("hello")
 
-i = hash("hello")
-insert(i, 9404797231)
-print(address_book[i])
