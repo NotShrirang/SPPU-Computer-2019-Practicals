3c2bbb2b5d1b8b05e77f9271a913a6fe7c2f7beb
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-04-07 16:37:05 +0530

    Update Assignment1.py

--- Assignment1.py
+++ Assignment1.py
@@ -4,7 +4,6 @@
 # Input : 
 #   N clients
 # For each client : Name & Telephone No.
-import sys
 def hash(num) -> int:
     return int(num % total_numbers)
 def linearProbingWithoutReplacement(h) -> int:
@@ -40,6 +39,15 @@ def displayHashTable():
     for bucket in hashTable:
         print(f"\t{i} : {bucket[0]} - {bucket[1]}")
         i += 1
+def isFull() -> bool:
+    count = 0
+    for bucket in hashTable:
+        if(bucket[0]!=None):
+            count += 1
+    if(count==total_numbers):
+        return True
+    else:
+        return False
 
 while(True):
     total_numbers = int(input("Enter total numbers :"))
@@ -53,15 +61,18 @@ while(True):
     choice = input("Enter Choice :\n1. Insert new number\n2. Display\n3. Exit\n-->")
     try:
         match int(choice):
-            case 1 : 
-                name = input("Name : ")
-                while(True):
-                    number = int(input("Num : "))
-                    if(number<=999999999):
-                        print("Invalid Number! Please re-enter!")
-                    else:
-                        break
-                insert(name, number)
+            case 1 :
+                if(isFull()):
+                    print("Hash Table is full!")
+                else:
+                    name = input("Name : ")
+                    while(True):
+                        number = int(input("Num : "))
+                        if(number<=999999999):
+                            print("Invalid Number! Please re-enter!")
+                        else:
+                            break
+                    insert(name, number)
             case 2 : 
                 displayHashTable()
             case 3: 
