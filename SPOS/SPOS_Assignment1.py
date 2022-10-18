ffbe768936a63783ff3f0ea4cb831867c9ded0fc
Shrirang Mahajan <85283622+NotShrirang@users.noreply.github.com>
2022-10-18 09:07:20 +0530

    Update SPOS_Assignment1.py

--- SPOS/SPOS_Assignment1.py
+++ SPOS/SPOS_Assignment1.py
@@ -1,8 +1,10 @@
-KEYWORDS = ["START", "DC", "DW", "END"]
+import sys
+
+KEYWORDS = ["START", "DC", "DW", "LTORG", "MOVER", "END"]
 AD = ["START", "END", "LTORG"]
 DS = ["DC", "DW"]
-IS = []
-
+IS = ["MOVER"]
+OPCODETABLE = {}
 
 class SymbolTableEntry():
     def __init__(self, symbolName: str, address: int) -> None:
@@ -36,6 +38,8 @@ class Assembler():
 
     def pass1(self) -> None:
         with open(self.filepath, "r") as f:
+            print("Code:\n" + f.read(), "\n")
+        with open(self.filepath, "r") as f:
             self.code = f.read().split("\n")
         
         for cnt, line in enumerate(self.code):
@@ -45,13 +49,18 @@ class Assembler():
             if cnt == 0 and words[0] != "START":
                 raise(Exception("Program should always start with START keyword."))
             self.parse(words, cnt)
-            self.current_address = int(self.current_address) + 1
 
+        print("\nSymTab:")
         for symb in self.symTab:
             print(symb.symbol_name, symb.address)
         
+        print("\nLitTab:")
         for symb in self.litTab:
             print(symb.literal_name, symb.address)
+        
+        print("\nPoolTab:")
+        for cnt, pool in enumerate(self.poolTab):
+            print(f"#{cnt}", pool.index)
         
         with open("output.txt", "w") as f:
             f.write("SymTab: \n")
@@ -66,22 +75,27 @@ class Assembler():
 
     def parse(self, words: list[str], address: int):
         if words[0] in KEYWORDS:
+            # print(self.current_address)
             if words[0] in AD:
+                # print(f"{self.current_address=}")
                 if words[0] == "START":
-                    self.startingAddr = words[1]
+                    self.startingAddr = int(words[1])
                     self.current_address = self.startingAddr
+                    self.current_address = int(self.current_address) + 1
                 elif words[0] == "END":
                     p = PoolTableEntry(index=len(self.litTab))
-                    print(f"end : {len(self.litTab)=}")
+                    # print(f"end : {len(self.litTab)=}")
                     self.poolTab.append(p)
                     self.litTab.extend(self.literal_buffer)
                     self.literal_buffer = []
                     self.end = True
-                elif word[0] == "LTORG":
+                    self.current_address = int(self.current_address) + 1
+                elif words[0] == "LTORG":
                     for lit in self.literal_buffer:
+                        # print(f"{self.current_address=}")
                         lit.address = self.current_address
                     p = PoolTableEntry(index=len(self.litTab))
-                    print(f"ltorg : {len(self.litTab)=}")
+                    # print(f"ltorg : {len(self.litTab)=}")
                     self.poolTab.append(p)
                     self.litTab.extend(self.literal_buffer)
                     self.literal_buffer = []
@@ -90,20 +104,25 @@ class Assembler():
             elif words[0] in DS:
                 s = SymbolTableEntry(symbolName=words[1], address = (int(self.startingAddr) + int(address)))
                 self.symTab.append(s)
+                self.current_address = int(self.current_address) + 1
             elif words[0] in IS:
+                self.current_address = int(self.current_address) + 1
                 pass
         for word in words:
             index = word.find('"=') # if it is literal
             if index != -1:
                 literal = word[index+2 : -1]
                 # print(literal)
-                l = LiteralTableEntry(litName=literal, address=(int(self.startingAddr) + int(address)))
+                l = LiteralTableEntry(litName=literal, address=(int(self.startingAddr) + int(self.current_address)))
+                # print(f"Literal Address : {l.address=}")
+                self.current_address = int(self.current_address) + 1
                 self.literal_buffer.append(l)
 
     def pass2(self):
         pass                
 
 if __name__ == "__main__":
-    a = Assembler("asm1.txt")
+    a = Assembler(sys.argv[1])
+    # a = Assembler("asm1.txt")
     a.pass1()
     a.pass2()
